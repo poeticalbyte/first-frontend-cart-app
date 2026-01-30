645706b792135ac287e1a134ac8e19e6381c3fae
poeticalbyte <poeticalbyte@gmail.com>
2026-01-29 19:45:30 -0500

    feat: replace local product data with backend fetch and added a little loading UI
    Replace local data.js with API calls, update Catalog to load products asynchronously and show loading state when loading the products.

--- src/features/catalog/Catalog.jsx
+++ src/features/catalog/Catalog.jsx
@@ -5,14 +5,24 @@ import { CatalogItem } from "./CatalogItem";
 export const Catalog = ({ handler }) => {
 
   const [ products, setProducts ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(true); 
+
+  const findAll = async () => {
+    const prods = await getProducts();
+    setProducts(prods);
+    setIsLoading(false);
+  };
 
   useEffect(
       () => {
-      setProducts(getProducts());
+        findAll();
     }, []);
 
   return (
     <>
+      {
+        isLoading && <div className="alert alert-info">Loading...</div>
+      }
       <div className="row">
         { products.map(prod => (
           <div className="col-4 my-2" key={prod.id}>
--- src/features/catalog/productService.js
+++ src/features/catalog/productService.js
@@ -1,6 +1,9 @@
-import { products } from "./products";
 
-export const getProducts = () => {
+export const getProducts = async () => {
+
+  const response = await fetch('http://localhost:8080/products');
+
+  const products = await response.json();
 
   return products;
 }
